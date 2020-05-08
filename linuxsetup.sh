f5a7789a89597b4617a6b3b864b0f4334321ac1f
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-05-09 06:24:30 +0700

    Update linuxsetup.sh

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -8,23 +8,6 @@
 
 # Install packages
 command -v pacman > /dev/null
-if [[ $? != 1 ]]; then
-        # The OS have pacman, it is probably Arch!
-	sudo pacman -S zsh adb fastboot curl git code neofetch iwd dhcpcd gnome-terminal lightdm gnome-backgrounds lightdm-gtk-greeter telegram-desktop ttf-opensans inetutils
-
-        # Install aurpkg
-        git clone --depth=1 https://aur.archlinux.org/aurpkg.git /tmp/aurpkg
-        cd /tmp/aurpkg
-        makepkg -si
-        cd ~
-
-        # Install Google Chrome
-        aurpkg -S google-chrome
-
-else
-        # Apart from Arch, We only do Debian/Ubuntu.
-	sudo apt-get -y install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gnupg2 gperf imagemagick lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev zsh apt-utils
-fi
 
 # Run oh-my-zsh installer unatteneded
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
