de267c0ff7052176068bb43b88f47a5f3228304e
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-05-09 05:29:54 +0700

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -11,18 +11,14 @@
 # Export KERNELDIR as en environment-wide thingy
 # We start in scripts, so like, don't clone things there
 KERNELDIR="$(pwd)"
-SCRIPTS=${KERNELDIR}/kernelscripts
+SCRIPTS=${KERNELDIR}/scripts
 OUTDIR=${KERNELDIR}/out
 
 # Pick your poison
-if [[ "$*" =~ "clang"* ]]; then
-        git clone https://github.com/kdrag0n/proton-clang --depth=1 "${KERNELDIR}"/clang
-        COMPILER_STRING='Proton Clang (latest)'
+        COMPILER_STRING='Avalon Clang (latest)'
 	COMPILER_TYPE='clang'
 else
         # Default to GCC from Arter
-        git clone https://github.com/arter97/arm64-gcc --depth=1 "${KERNELDIR}/gcc"
-        git clone https://github.com/arter97/arm32-gcc --depth=1 "${KERNELDIR}/gcc32"
         COMPILER_STRING='GCC 9.x'
 	COMPILER_TYPE='GCC9.x'
 fi
