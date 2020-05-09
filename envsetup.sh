2ed81a045a11e1c3d14022e945c8737cf87f06d5
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-05-09 22:34:27 +0700

    Try Fix Clang

--- envsetup.sh
+++ envsetup.sh
@@ -14,16 +14,14 @@ KERNELDIR="$(pwd)"
 SCRIPTS=${KERNELDIR}/kernelscripts
 OUTDIR=${KERNELDIR}/out
        
-        git clone https://github.com/Haseo97/Avalon-Clang-11.0.1.git --depth=1 "${KERNELDIR}"/clang
-        COMPILER_STRING='Avalon Clang (latest)'
-	COMPILER_TYPE='clang'
-else
-        # Default to GCC from Arter
-        git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi.git --depth=1 -b 05022020-9.2.1 "${KERNELDIR}/gcc32"
-        git clone https://github.com/baalajimaestro/aarch64-maestro-linux-android.git --depth=1 -b 05022020-9.2.1 "${KERNELDIR}/gcc"
-        COMPILER_STRING='GCC 9.x'
-	COMPILER_TYPE='GCC9.x'
-fi
+git clone https://github.com/Haseo97/Avalon-Clang-11.0.1.git --depth=1 "${KERNELDIR}"/clang
+COMPILER_STRING='Avalon Clang (latest)'
+COMPILER_TYPE='clang'
+# Default to GCC from Arter
+git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi.git --depth=1 -b 05022020-9.2.1 "${KERNELDIR}/gcc32"
+git clone https://github.com/baalajimaestro/aarch64-maestro-linux-android.git --depth=1 -b 05022020-9.2.1 "${KERNELDIR}/gcc"
+COMPILER_STRING='GCC 9.x'
+COMPILER_TYPE='GCC9.x'
 
 export COMPILER_STRING COMPILER_TYPE KERNELDIR SCRIPTS OUTDIR
 
