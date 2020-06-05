94ed0c849cb4557f18f547b35273db6c23021e48
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-06-06 02:57:00 +0700

    Let's try azure clang

--- envsetup.sh
+++ envsetup.sh
@@ -16,13 +16,15 @@ OUTDIR=${KERNELDIR}/out
        
 #git clone https://github.com/kdrag0n/proton-clang.git --depth=1 "${KERNELDIR}"/clang
 #git clone https://github.com/Haseo97/Avalon-Clang-11.0.1.git --depth=1 "${KERNELDIR}"/clang
+git clone https://github.com/Panchajanya1999/azure-clang.git --depth=1 "${KERNELDIR}"/clang
 #COMPILER_STRING='Proton Clang'
-#COMPILER_TYPE='clang'
+COMPILER_STRING='Azure Clang'
+COMPILER_TYPE='clang'
 # Default to GCC from Arter
-git clone https://github.com/Haseo97/aarch64-linux-gnu.git --depth=1 "${KERNELDIR}/gcc"
-git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi.git --depth=1 -b 07032020-9.2.1 "${KERNELDIR}/gcc32"
-COMPILER_STRING='GCC 10'
-COMPILER_TYPE='gcc'
+#git clone https://github.com/Haseo97/aarch64-linux-gnu.git --depth=1 "${KERNELDIR}/gcc"
+#git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi.git --depth=1 -b 07032020-9.2.1 "${KERNELDIR}/gcc32"
+#COMPILER_STRING='GCC 10'
+#COMPILER_TYPE='gcc'
 
 export COMPILER_STRING COMPILER_TYPE KERNELDIR SCRIPTS OUTDIR
 
